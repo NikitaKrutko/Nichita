12e9b454987575843a66df9ab4748244b71fb775
NikitaKrutko <lenanici280@mail.ru>
2023-09-27 15:59:14 +0300

    initial commit

--- 27.09.2023/ПР4.py
+++ 27.09.2023/ПР4.py
@@ -1,16 +1,16 @@
-# a = int(input())
-# b = int(input())
-# for i in range(a, b + 1):
-#     print(i)
+a = int(input())
+b = int(input())
+for i in range(a, b + 1):
+    print(i)
 
-# n = int(input())
-# if n <= 9:
-#     for i in range(1, n+ 1):
-#         for a in range(1, i+1):
-#             print(a, sep="",end="")
-#         print()
-# else:
-#     print("Try again")
+n = int(input())
+if n <= 9:
+    for i in range(1, n+ 1):
+        for a in range(1, i+1):
+            print(a, sep="",end="")
+        print()
+else:
+    print("Try again")
 
 n = int(input())
 z = 0
