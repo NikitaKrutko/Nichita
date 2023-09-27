0ad847f4ff41ad163c15958ed62ac9ae5b021111
NikitaKrutko <lenanici280@mail.ru>
2023-09-27 15:58:42 +0300

    initial commit

---
+++ 27.09.2023/ПР4.py
@@ -0,0 +1,46 @@
+# a = int(input())
+# b = int(input())
+# for i in range(a, b + 1):
+#     print(i)
+
+# n = int(input())
+# if n <= 9:
+#     for i in range(1, n+ 1):
+#         for a in range(1, i+1):
+#             print(a, sep="",end="")
+#         print()
+# else:
+#     print("Try again")
+
+n = int(input())
+z = 0
+for i in range(1, n+1):
+    z += i**3
+    print(i, i ** 3, z)
+print(z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
